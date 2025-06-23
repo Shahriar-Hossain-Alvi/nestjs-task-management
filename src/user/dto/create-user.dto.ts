a12583eea15cebeeb706fef61eb2c4b83f07c09e
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-06-23 23:32:37 +0600

    created services and DTOs for users

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,22 @@
-export class CreateUserDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Role } from '@prisma/client';
+import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  fullName!: string;
+
+  @IsString()
+  phone: string;
+
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsEnum(Role)
+  role: Role;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) {}
+
+  // create a user
+  async create(createUserDto: CreateUserDto) {
+    return await this.prisma.user.create({
+      data: createUserDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  // get all users
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  // get one user
+  async findOne(id: number) {
+    return await this.prisma.user.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    return await this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
