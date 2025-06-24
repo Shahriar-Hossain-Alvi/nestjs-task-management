358f3fb9a414c94acaeeaceeaf0d43cd403c65d4
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-06-24 20:32:02 +0600

    added userEntity for retured data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { plainToInstance } from 'class-transformer';
+import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -9,26 +11,33 @@ export class UserController {
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+    const user = this.userService.create(createUserDto);
+    return plainToInstance(UserEntity, user, { excludeExtraneousValues: true });
   }
 
   @Get()
   findAll() {
-    return this.userService.findAll();
+    // return this.userService.findAll();
+    const users = this.userService.findAll();
+    return plainToInstance(UserEntity, users, { excludeExtraneousValues: true });
   }
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.findOne(id);
+    // return this.userService.findOne(id);
+    const user = this.userService.findOne(id);
+    return plainToInstance(UserEntity, user, { excludeExtraneousValues: true });
   }
 
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(id, updateUserDto);
+    const updatedUser = this.userService.update(id, updateUserDto);
+    return plainToInstance(UserEntity, updatedUser, { excludeExtraneousValues: true });
   }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
-    return this.userService.remove(id);
+    const deletedUser = this.userService.remove(id);
+    return plainToInstance(UserEntity, deletedUser, { excludeExtraneousValues: true });
   }
 }
